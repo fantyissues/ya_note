bf717f217e3f5016870d627f8ff8a1e91ba5a37f
Denis Lopin <denis.lopin.w@yandex.ru>
2023-10-18 00:03:04 +0500

    Исправить тесты маршрутов

--- notes/tests/test_routes.py
+++ notes/tests/test_routes.py
@@ -10,7 +10,6 @@ User = get_user_model()
 
 
 class TestRoutes(TestCase):
-    SLUG = 'test-note-slug'
 
     @classmethod
     def setUpTestData(cls):
@@ -19,26 +18,9 @@ class TestRoutes(TestCase):
         cls.note = Note.objects.create(
             title='Заголовок заметки',
             text='Текст заметки',
-            slug=cls.SLUG,
+            slug='test-note-slug',
             author=cls.author,
         )
-
-    def test_reverse_view_name(self):
-        urls = (
-            ('/', 'notes:home', None),
-            ('/add/', 'notes:add', None),
-            (f'/edit/{self.SLUG}/', 'notes:edit', (self.SLUG,)),
-            (f'/note/{self.SLUG}/', 'notes:detail', (self.SLUG,)),
-            (f'/delete/{self.SLUG}/', 'notes:delete', (self.SLUG,)),
-            ('/notes/', 'notes:list', None),
-            ('/done/', 'notes:success', None),
-            ('/auth/login/', 'users:login', None),
-            ('/auth/logout/', 'users:logout', None),
-            ('/auth/signup/', 'users:signup', None),
-        )
-        for path, view_name, args in urls:
-            with self.subTest(path=path, view_name=view_name):
-                self.assertEqual(path, reverse(view_name, args=args))
 
     def test_pages_availability(self):
         view_names = (
@@ -69,13 +51,14 @@ class TestRoutes(TestCase):
     def test_redirect_for_anonymous_client(self):
         view_names_args = (
             ('notes:list', None),
+            ('notes:add', None),
             ('notes:detail', (self.note.slug,)),
             ('notes:edit', (self.note.slug,)),
             ('notes:delete', (self.note.slug,)),
         )
         login_url = reverse('users:login')
         for name, args in view_names_args:
-            with self.subTest(name=name, args=args):
+            with self.subTest(name=name):
                 url = reverse(name, args=args)
                 redirect_url = f'{login_url}?next={url}'
                 self.assertRedirects(self.client.get(url), redirect_url)
