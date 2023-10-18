881978a5087b6b23a8444c9f13f21e61e3986e87
Denis Lopin <denis.lopin.w@yandex.ru>
2023-10-18 15:44:26 +0500

    Добавить тесты маршрутов pytest

---
+++ notes/pytest_tests/conftest.py
@@ -0,0 +1,30 @@
+import pytest
+
+from notes.models import Note
+
+
+@pytest.fixture
+def author(django_user_model):
+    return django_user_model.objects.create(username='Автор')
+
+
+@pytest.fixture
+def author_client(author, client):
+    client.force_login(author)
+    return client
+
+
+@pytest.fixture
+def note(author):
+    note = Note.objects.create(
+        title='Заголовок',
+        text='Текст заметки',
+        slug='note-slug',
+        author=author,
+    )
+    return note
+
+
+@pytest.fixture
+def slug_for_args(note):
+    return note.slug,
---
+++ notes/pytest_tests/test_routes.py
@@ -0,0 +1,60 @@
+from http import HTTPStatus
+
+import pytest
+from django.urls import reverse
+from pytest_django.asserts import assertRedirects
+
+
+@pytest.mark.parametrize(
+        'name',
+        ('notes:home', 'users:login', 'users:logout', 'users:signup'),
+)
+def test_pages_availablility_for_anonymous_user(client, name):
+    response = client.get(reverse(name))
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'name',
+    ('notes:list', 'notes:add', 'notes:success'),
+)
+def test_pages_availability_for_auth_user(admin_client, name):
+    response = admin_client.get(reverse(name))
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'parametrized_client, expected_status',
+    (
+        (pytest.lazy_fixture('admin_client'), HTTPStatus.NOT_FOUND),
+        (pytest.lazy_fixture('author_client'), HTTPStatus.OK)
+    )
+)
+@pytest.mark.parametrize(
+    'name',
+    ('notes:detail', 'notes:edit', 'notes:delete'),
+)
+def test_pages_availability_for_author(
+    parametrized_client, name, note, expected_status,
+):
+    response = parametrized_client.get(reverse(name, args=(note.slug,)))
+    assert response.status_code == expected_status
+
+
+@pytest.mark.parametrize(
+    'name, args',
+    (
+        ('notes:detail', pytest.lazy_fixture('slug_for_args')),
+        ('notes:edit', pytest.lazy_fixture('slug_for_args')),
+        ('notes:delete', pytest.lazy_fixture('slug_for_args')),
+        ('notes:add', None),
+        ('notes:success', None),
+        ('notes:list', None),
+    ),
+)
+def test_redirects(client, name, args):
+    login_url = reverse('users:login')
+    url = reverse(name, args=args)
+    expected_url = f'{login_url}?next={url}'
+    response = client.get(url)
+    assertRedirects(response, expected_url)
