e335513bda254785b9d9537e60e667b8c23d597d
Denis Lopin <denis.lopin.w@yandex.ru>
2023-10-18 22:42:30 +0500

    Добавить тесты контента

---
+++ notes/pytest_tests/test_content.py
@@ -0,0 +1,29 @@
+import pytest
+from django.urls import reverse
+
+
+@pytest.mark.parametrize(
+    'parametrized_client, note_in_list',
+    (
+        (pytest.lazy_fixture('author_client'), True),
+        (pytest.lazy_fixture('admin_client'), False),
+    ),
+)
+def test_notes_list_for_different_users(
+    note, parametrized_client, note_in_list,
+):
+    response = parametrized_client.get(reverse('notes:list'))
+    object_list = response.context['object_list']
+    assert (note in object_list) is note_in_list
+
+
+@pytest.mark.parametrize(
+    'name, args',
+    (
+        ('notes:add', None),
+        ('notes:edit', pytest.lazy_fixture('slug_for_args')),
+    ),
+)
+def test_pages_contains_form(author_client, name, args):
+    response = author_client.get(reverse(name, args=args))
+    assert 'form' in response.context
--- notes/tests/test_content.py
+++ notes/tests/test_content.py
@@ -8,45 +8,38 @@ from notes.models import Note
 User = get_user_model()
 
 
-class TestNotesListPage(TestCase):
-    NOTES_AMOUNT = 100
+class TestContent(TestCase):
     HOME_URL = reverse('notes:list')
 
     @classmethod
     def setUpTestData(cls):
-        cls.user = User.objects.create(username='Автор')
-        cls.user_client = Client()
-        cls.user_client.force_login(cls.user)
-        Note.objects.bulk_create(
-            Note(
-                title=f'Заметка {index}',
-                text='Текст заметки',
-                author=cls.user,
-                slug=f'note-{index}',
-            )
-            for index in range(cls.NOTES_AMOUNT)
+        cls.author = User.objects.create(username='Автор')
+        cls.author_client = Client()
+        cls.author_client.force_login(cls.author)
+        cls.user = User.objects.create(username='Пользователь')
+        cls.note = Note.objects.create(
+            title='Заголовок заметки',
+            text='Текст заметки',
+            slug='note-slug',
+            author=cls.author,
         )
 
-    def test_notes_count(self):
-        response = self.user_client.get(self.HOME_URL)
-        object_list = response.context['object_list']
-        self.assertEqual(len(object_list), self.NOTES_AMOUNT)
+    def test_notes_list_for_author(self):
+        response = self.author_client.get(self.HOME_URL)
+        self.assertIn(self.note, response.context['object_list'])
 
+    def test_notes_list_for_not_author(self):
+        self.client.force_login(self.user)
+        response = self.client.get(self.HOME_URL)
+        self.assertNotIn(self.note, response.context['object_list'])
 
-class TestNoteAddPage(TestCase):
-
-    @classmethod
-    def setUpTestData(cls):
-        cls.user = User.objects.create(username='Автор')
-        cls.user_client = Client()
-        cls.user_client.force_login(cls.user)
-        cls.add_url = reverse('notes:add')
-        cls.form_data = {
-            'title': 'Заголовок',
-            'text': 'Текст',
-        }
-
-    def test_note_add_has_form(self):
-        response = self.user_client.get(self.add_url)
-        self.assertIn('form', response.context)
-        self.assertIsInstance(response.context['form'], NoteForm)
+    def test_pages_contains_form(self):
+        names_args = (
+            ('notes:add', None),
+            ('notes:edit', (self.note.slug,))
+        )
+        for name, args in names_args:
+            with self.subTest(name=name):
+                response = self.author_client.get(reverse(name, args=args))
+                self.assertIn('form', response.context)
+                self.assertIsInstance(response.context['form'], NoteForm)
