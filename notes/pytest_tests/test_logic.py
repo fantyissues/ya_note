64c09f65db3c324195b6b78f5f2ac9c0efaa69dc
Denis Lopin <denis.lopin.w@yandex.ru>
2023-10-19 01:05:44 +0500

    Добавить тесты логики

---
+++ notes/pytest_tests/test_logic.py
@@ -0,0 +1,26 @@
+import pytest
+from django.urls import reverse
+from pytest_django.asserts import assertRedirects
+
+from notes.models import Note
+
+
+def test_user_can_create_note(author_client, author, form_data):
+    response = author_client.post(reverse('notes:add'), data=form_data)
+    assertRedirects(response, reverse('notes:success'))
+    assert Note.objects.count() == 1
+    new_note = Note.objects.get()
+    assert new_note.title == form_data['title']
+    assert new_note.text == form_data['text']
+    assert new_note.slug == form_data['slug']
+    assert new_note.author == author
+
+
+@pytest.mark.django_db
+def test_anonymous_user_cant_create_note(client, form_data):
+    url = reverse('notes:add')
+    response = client.post(url, data=form_data)
+    login_url = reverse('users:login')
+    expected_url = f'{login_url}?next={url}'
+    assertRedirects(response, expected_url)
+    assert Note.objects.count() == 0
